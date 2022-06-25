fca0ae5dbc6e33e448db6f5ab150a5cd5ab61434
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-25 20:26:57 +0100

    Time is called

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 function App() {
-  function time() {
-    let time = new Date().toLocaleTimeString();
-    console.log(time);
-  }
+  let time = new Date().toLocaleTimeString();
+  console.log(time);
+
+  function Timer() {}
   return (
     <div className="container">
-      <h1>TIME</h1>
+      <h1>{time}</h1>
       <button>Get Time</button>
     </div>
   );
