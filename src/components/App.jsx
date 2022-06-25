365e5524d2f04ff03edb14a88fdef2d9bcddbd84
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-25 20:22:37 +0100

    Timer countdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function App() {
+  function time() {
+    let time = new Date().toLocaleTimeString();
+    console.log(time);
+  }
   return (
     <div className="container">
       <h1>TIME</h1>
